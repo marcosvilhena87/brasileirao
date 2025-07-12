a27561ff028633ac5807125aabfd3d1f3ce97f71
marcosvilhena87 <105467394+marcosvilhena87@users.noreply.github.com>
2025-07-12 11:26:17 -0300

    Add Dixon-Coles rating method

--- src/brasileirao/__init__.py
+++ src/brasileirao/__init__.py
@@ -1,6 +1,16 @@
 """Convenience exports for the simulator package."""
 
-from .simulator import league_table, parse_matches, simulate_chances
+from .simulator import (
+    league_table,
+    parse_matches,
+    simulate_chances,
+    estimate_dixon_coles_strengths,
+)
 
-__all__ = ["parse_matches", "league_table", "simulate_chances"]
+__all__ = [
+    "parse_matches",
+    "league_table",
+    "simulate_chances",
+    "estimate_dixon_coles_strengths",
+]
 
